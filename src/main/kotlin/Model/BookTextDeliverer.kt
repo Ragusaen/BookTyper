342faec989de7af4f0b50ca6eff64c198c5fc638
Ragusaen <rtollu18@student.aau.dk>
2020-10-12 16:37:36 +0200

    intermediate

--- src/main/kotlin/Model/BookTextDeliverer.kt
+++ src/main/kotlin/Model/BookTextDeliverer.kt
@@ -1,22 +1,29 @@
 package Model
 
+import nl.siegmann.epublib.domain.Book as EpubBook
+import nl.siegmann.epublib.domain.Resource
 import nl.siegmann.epublib.epub.EpubReader
 import java.io.FileInputStream
 
 class BookTextDeliverer(epubFileStream: FileInputStream) : ITextDeliverer {
+    private class BookIterator(book: EpubBook) {
+        private val contents = book.contents
+        private var index = 0
+
+        val current get() = contents[index]
+
+        fun advance() {
+            index = (index + 1) % contents.size
+        }
+    }
     private val book = EpubReader().readEpub(epubFileStream)
 
+    private val bookIterator = BookIterator(book)
+
     override val title = book.title
+    override val currentSection = bookIterator.current.title
 
-    override val currentSection = "ars"
 
-    private object BookIterator {
-        private val index = 0
-        val current = 
-    }
-
-    private val contentIterator = book.contents.iterator()
-
-    override fun next(): String = contentIterator.next().mediaType.name
+    override fun next(): String = bookIterator.apply { advance() }.current.reader.readText()
 
 }
