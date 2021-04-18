6d8edf72afce1a92069c6af8ab0f089525bd80c8
Ragusaen <rtollu18@student.aau.dk>
2021-04-18 10:22:16 +0200

    Added remove book option

--- src/main/kotlin/view/TextSelectionView.kt
+++ src/main/kotlin/view/TextSelectionView.kt
@@ -2,8 +2,11 @@ package view
 
 import controller.TextSelectionController
 import javafx.geometry.Pos
+import javafx.scene.control.Alert
+import javafx.scene.control.ButtonType
 import javafx.scene.image.Image
 import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import javafx.scene.text.Font
 import model.Book
@@ -17,6 +20,8 @@ class TextSelectionView : View() {
 
     val controller = TextSelectionController()
 
+    lateinit var updateBooks: () -> Unit
+
     init {
         with(root) {
 
@@ -25,43 +30,77 @@ class TextSelectionView : View() {
 
             val booksBox = vbox {}
 
-            val updateBooks = {
+            updateBooks = {
                 booksBox.clear()
                 for (book in UserData.current.books)
                     booksBox.add(bookElement(book))
             }
             updateBooks()
 
-            add(bookElement("Add new book", Image("file:" + this::class.java.getResource("/addbook.png").path))
-                {controller.addBook(); updateBooks()})
+            println(this::class.java.getResource("/addbook.png").path)
+            add(bookElement("Add new book", Image(this.javaClass.getResourceAsStream("/addbook.png")),
+                {controller.addBook(); updateBooks()}))
 
             add(booksBox)
         }
     }
 
-    private fun bookElement(book: Book): HBox = bookElement(book.title, Image(FileInputStream(book.coverPath))) { chooseText(book) }
+    private fun bookElement(book: Book): HBox = bookElement(book.title, Image(FileInputStream(book.coverPath)), { chooseText(book) }, { deleteBook(book)} )
 
-    private fun bookElement(title: String, cover: Image, action: () -> Unit): HBox = hbox {
+    private fun bookElement(title: String, cover: Image, action: () -> Unit, delAction: (() -> Unit)? = null): HBox = hbox {
         paddingAll = 5.0
-        imageview(cover) {
-            isPreserveRatio = true
-            fitHeight = 200.0
-        }
+        hbox {
+            imageview(cover) {
+                isPreserveRatio = true
+                fitHeight = 200.0
 
-        vbox {
-            paddingAll = 50.0
-            alignment = Pos.CENTER
-            label(title) {
-                font = Font(48.0)
             }
+
+            vbox {
+                paddingAll = 50.0
+                alignment = Pos.CENTER
+                label(title) {
+                    font = Font(48.0)
+                }
+            }
+            this.setOnMouseClicked { event ->
+                if (event.button.ordinal == 1)
+                    action()
+            }
+            hgrow = Priority.ALWAYS
         }
-        this.setOnMouseClicked { event ->
-            if (event.button.ordinal == 1)
-                action()
+        if (delAction != null) {
+            vbox {
+                alignment = Pos.CENTER_RIGHT
+                imageview(Image(this.javaClass.getResourceAsStream("/removebook.png"))) {
+                    fitHeight = 50.0
+                    fitWidth = 50.0
+                }
+                paddingRight = 15.0
+                paddingLeft = 15.0
+                this.setOnMouseClicked { event ->
+                    if (event.button.ordinal == 1)
+                        delAction()
+                }
+            }
         }
     }
 
     private fun chooseText(book: Book) {
         replaceWith(find<TypingView>(TypingScope(BookTextDeliverer(book))))
+    }
+
+    private fun deleteBook(book: Book) {
+        alert(Alert.AlertType.CONFIRMATION, "Delete ${book.title}?",
+            "Are you sure you want to delete ${book.title}?\nThis does not delete the epub file from your drive," +
+                    " only removes it from BookTyper.", ButtonType.YES, ButtonType.NO, actionFn = { bt: ButtonType ->
+                if (bt == ButtonType.YES) {
+                    UserData.current.books.remove(book)
+                    UserData.saveCurrent()
+                    updateBooks()
+                }
+            })
+
+
     }
 }
--- src/main/kotlin/view/TyperView.kt
+++ src/main/kotlin/view/TyperView.kt
@@ -3,11 +3,15 @@ package view
 import controller.TyperController
 import model.*
 import javafx.application.Platform
+import javafx.css.Style
 import javafx.scene.input.KeyEvent
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.text.TextFlow
+import org.fxmisc.richtext.InlineCssTextArea
+import org.fxmisc.richtext.StyleClassedTextArea
+import org.fxmisc.richtext.StyledTextArea
 import tornadofx.*
 
 class TyperScope(val textDeliverer: ITextDeliverer, val textHighlighter: ITypingTextHighlighter, val updateMetaText: (String) -> Unit) : Scope()
@@ -38,11 +42,11 @@ class TyperView() : View() {
 
             vbox {
                 border = Border(BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))
-
-                typeText = textflow() {
-                    paddingAll = 5.0
-                    style(append = true) {
-                        backgroundColor += Color.WHITE
+                minHeight = 100.0
+                typeText = textflow().apply {
+                    minHeight = 500.0
+                    stylesheet {
+                        addClass(".Incorrect { -fx:fill: green; })")
                     }
                 }
             }
@@ -66,13 +70,14 @@ class TyperView() : View() {
 
         for (htext in controller.highlightedTextSegments.filter { it.text.isNotEmpty() }) {
             val t = if (htext.highlight == TextHighlight.Incorrect)
-                            htext.text.replace(' ', '_')
-                        else htext.text
+                htext.text.replace(' ', '_')
+            else htext.text
 
             typeText.text(t) {
                 fill = highlightToColor(htext.highlight)
                 font = Font.font("Monospace", 24.0)
             }
         }
+        typeText.minHeight = 100.0
     }
 }
